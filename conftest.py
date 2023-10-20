581366c41c814abeafbe6307ab4f9bf194d8e594
Pavel Kulyov <kulyov.pavel@gmail.com>
2023-10-20 23:30:28 +0300

    tests: fix Module initialization
    Modern version of pytest requires Module to be initialized using
    factory method `.from_parent(parent, *, ...)`.

--- conftest.py
+++ conftest.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import hy  # This import is mandatory for pytest to load hy modules.
 from _pytest.python import Module
 
 
 def pytest_collect_file(path, parent):
     if path.ext == ".hy" and "test_" in path.basename:
-        return Module(path, parent)
+        return Module.from_parent(parent, path=Path(path))
